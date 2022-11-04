9cd66c43f344c91c3596248926b227344173005f
rabobahago <rabobahago@gmail.com>
2022-11-04 11:38:14 +0100

    get balance async function added in buy coffee script

--- scripts/buy-coffee.js
+++ scripts/buy-coffee.js
@@ -5,6 +5,11 @@
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
 
+async function getBalance(address) {
+  const balanceBigInt = await hre.waffle.ethers.provider.getBalance(address);
+  return hre.ethers.utils.formatEther(balanceBigInt);
+}
+
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
   // line interface.
