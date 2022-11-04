8d7ab6bcf78032578c4276ae4ba4fd4a05b004e3
rabobahago <rabobahago@gmail.com>
2022-11-04 15:11:41 +0100

    deploy script added

---
+++ scripts/deploy.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+async function main() {
+  const BuyMeACoffee = await hre.ethers.getContractFactory("BuyMeACoffee");
+  const buyMeACoffee = await BuyMeACoffee.deploy();
+  await buyMeACoffee.deployed();
+  console.log(`BuyMeACoffee contract deployed to ${buyMeACoffee.address}`);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
