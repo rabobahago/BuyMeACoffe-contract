b160319f8754f1a5cb036bb3e29c7ffc21e6b0a0
rabobahago <rabobahago@gmail.com>
2022-11-05 15:44:50 +0100

    next react react-dom installed to spin up the frontend

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,10 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//0x15c65b9589D01273bd4Ee8e6Ca57A01C10115f64
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -18,4 +14,13 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  */
 module.exports = {
   solidity: "0.8.0",
+  defaultNetwork: "goerli",
+  networks: {
+    hardhat: {},
+    goerli: {
+      chainId: 5,
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
